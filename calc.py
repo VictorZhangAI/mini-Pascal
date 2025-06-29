d431e331c36ba11f1bc25f6b13fbf2c94d4d13b4
VZstless <2319725508@qq.com>
2025-06-29 09:52:10 +0800

    part of parser

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
+INTEGER, MUL, DIV, EOF = 'INTEGER', 'MUL', 'DIV', 'EOF'
 
 class Token(object):
     def __init__(self, type, value):
@@ -7,22 +7,20 @@ class Token(object):
 
     def __str__(self):
         return 'Token({type}, {value})'.format(
-                    type = self.type,
-                    value = repr(self.value)
+            type = self.type
+            value = repr(self.value)
                 )
-
     def __repr__(self):
         return self.__str__()
 
-class Interpreter(object):
+class Lexer(object):
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        self.current_token = None
         self.current_char = self.text[self.pos]
 
     def error(self):
-        raise Exception('Error parsing input')
+        raise Exception('Invalid character')
 
     def advance(self):
         self.pos += 1
@@ -32,61 +30,7 @@ class Interpreter(object):
             self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-
-    def integer(self):
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        return int(result)
-
-    def get_next_token(self):
-        while self.current_char is not None:
-
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-            if self.current_char.isdigit():
-                return Token(INTEGER, self.integer())
-
-            if self.current_char == '+':
-                self.advance()
-                return Token(PLUS, '+')
-
-            if self.current_char == '-':
-                self.advance()
-                return Token(MINUS, '-')
-
-            self.error()
-            return Token(EOF, None)
-
-    def eat(self, token_type):
-        if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
-        else:
-            self.error()
-
-    def term(self):
-        token = self.current_token
-        self.eat(INTEGER)
-        return token.value
-
-    def expr(self):
-        self.current_token = self.get_next_token()
-        
-        result = self.term()
-        while self.current_token.type in (PLUS, MINUS):
-            token = self.current_token
-            if token.type == PLUS:
-                self.eat(PLUS)
-                result = result + self.term()
-            elif token.type == MINUS:
-                self.eat(MINUS)
-                result = result - self.term()
-
-        return result
+        while self.current_char is not None and self.current_char.is
 
 def main():
     while True:
@@ -96,7 +40,8 @@ def main():
             break
         if not text:
             continue
-        interpreter = Interpreter(text)
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
 
